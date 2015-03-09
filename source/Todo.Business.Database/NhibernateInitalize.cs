ab3c849073cc46b342bd1429988f88a1bd6be98f
Paparow <mkakerow@googlemail.com>
2015-03-09 14:32:00 +0100

    removed tests

--- source/Todo.Business.Database/NhibernateInitalize.cs
+++ source/Todo.Business.Database/NhibernateInitalize.cs
@@ -37,22 +37,6 @@ namespace Todo.Business.Database
             SchemaUpdate up = new SchemaUpdate(cfg);
             up.Execute(true, true);
 
-            //
-            Todo.Business.Entities.Todo test1= new Entities.Todo();
-            test1.Title = "Test";
-            session.Save(test1);
-
-            Appointment test2 = new Appointment();
-            test2.Description = "Test";
-
-            session.Save(test2);
-
-            Contact test3 = new Contact();
-            test3.FirstName = "Test";
-
-            session.Save(test3);
-
-            List<Entities.Todo> todos = session.Query<Entities.Todo>().ToList();
         
             return cfg;
 
